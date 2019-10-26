406d870661a5e9dfaf44a40d959059fcc3982276
Lawrence Yao <lawry@debian8>
2019-10-26 19:06:37 +1100

    Implemented double click
    to allow selection details to be read without navigation occuring.

--- src/webvowl/js/modules/pickAndPin.js
+++ src/webvowl/js/modules/pickAndPin.js
@@ -4,7 +4,8 @@ var elementTools = require("../util/elementTools")();
 module.exports = function (){
   var pap = {},
     enabled = false,
-    pinnedElements = [];
+    pinnedElements = [],
+    navTargetIri = null;
 
   pap.addPinnedElement = function ( element ){
     // check if element is already in list
@@ -18,27 +19,37 @@ module.exports = function (){
     if ( !enabled ) {
       // FinanceIT@CSE: do my magic here.
       if (elementTools.isNode(selection)) {
-        var iri = selection.iri();
-        console.log("please load items related to " + iri);
+        let iri = selection.iri();
+        if (navTargetIri !== iri) {
+          // Selected something different, so do NOT navigate, but change target to this.
+          navTargetIri = iri;
+          console.log("Click the same node again to navigate to " + navTargetIri);
+        } else {
+          // Select the same node again, so navigate to it, and clear navTargetIri for next sequence.
+          navTargetIri = null;
+          console.log("Navigating to " + iri);
+          let bgloadUrl = "data/load?iri=" + encodeURIComponent(iri);
+          d3.xhr(bgloadUrl, "text/plain", function ( error, request ) {
+            if (error) {
+              console.log("Error loading data using " + bgloadUrl);
+            } else {
+              // Server should return us a JSON file name (without the extension)
+              // which we will use as the new fragment of the URL, which in turn
+              // causes a refresh of the graph.
+              let file_json = request.responseText;
+              console.log("Response from loading data is " + file_json);
 
-        var bgloadUrl = "data/load?iri=" + encodeURIComponent(iri);
-        d3.xhr(bgloadUrl, "text/plain", function ( error, request ) {
-          if (error) {
-            console.log("Error loading data using " + bgloadUrl);
-          } else {
-            // Server should return us a JSON file name (without the extension)
-            // which we will use as the new fragment of the URL, which in turn
-            // causes a refresh of the graph.
-            var file_json = request.responseText;
-            console.log("Response from loading data is " + file_json);
-
-            // Santy check, require file name to be less than 20 characters in case
-            // something other than the expected is returned.
-            if (file_json && file_json.length < 20) {
-              location.hash = encodeURIComponent(file_json);
+              // Santy check, require file name to be less than 20 characters in case
+              // something other than the expected is returned.
+              if (file_json && file_json.length < 20) {
+                location.hash = encodeURIComponent(file_json);
+              } else if (file_json === "") {
+                // Indicates no result for the selected entity.
+                window.alert("No results for\n\n" + iri);
+              }
             }
-          }
-        });
+          });
+        }
       }
       return;
     }
